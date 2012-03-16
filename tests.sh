0a961cb1c27b6e5dc4545758b1d88c5e73caf0ba
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2012-03-16 11:52:14 +0000

    for great victory

---
+++ tests.sh
@@ -0,0 +1,5 @@
+./processSUSUMinutes http://minutes.susu.org/files/enveth_minutes_27-10-2011.pdf
+./processSUSUMinutes http://minutes.susu.org/files/mdacom_minutes_16-12-2011.pdf
+./processSUSUMinutes http://minutes.susu.org/files/edwel_minutes_20-1-2005.pdf
+./processSUSUMinutes http://minutes.susu.org/files/umb_minutes_9-2-2011.pdf
+
